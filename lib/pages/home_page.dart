1365066adc51ae22e8c275874a2adc7e86e08c4a
Matheus Brevis <mattbrevis@gmail.com>
2023-01-20 11:04:19 -0300

    Add project

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+ 
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {   
+    return Scaffold(
+      appBar: AppBar(   
+        title: Text(widget.title),
+      ),
+      body: const Center(       
+        child: Text('Home Page')
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+}
