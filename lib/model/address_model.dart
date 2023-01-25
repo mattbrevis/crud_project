200748ee4f1509f495bbfef59c96b6383ab02f5b
Matheus Brevis <mattbrevis@gmail.com>
2023-01-24 23:51:48 -0300

    Creating new models

---
+++ lib/model/address_model.dart
@@ -0,0 +1,44 @@
+class AddressModel {
+  String? cep;
+  String? logradouro;
+  String? complemento;
+  String? bairro;
+  String? localidade;
+  String? uf;
+  String? ibge;
+  String? ddd;
+
+  AddressModel(
+      {this.cep,
+      this.logradouro,
+      this.complemento,
+      this.bairro,
+      this.localidade,
+      this.uf,
+      this.ibge,
+      this.ddd});
+
+  AddressModel.fromJson(Map<String, dynamic> json) {
+    cep = json['cep'];
+    logradouro = json['logradouro'];
+    complemento = json['complemento'];
+    bairro = json['bairro'];
+    localidade = json['localidade'];
+    uf = json['uf'];
+    ibge = json['ibge'];
+    ddd = json['ddd'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['cep'] = cep;
+    data['logradouro'] = logradouro;
+    data['complemento'] = complemento;
+    data['bairro'] = bairro;
+    data['localidade'] = localidade;
+    data['uf'] = uf;
+    data['ibge'] = ibge;
+    data['ddd'] = ddd;
+    return data;
+  }
+}
